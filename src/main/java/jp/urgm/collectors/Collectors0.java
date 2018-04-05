206e9fa24eaf5a69d75545dafffad63ec4128976
Taichi Uragami <backpaper0@gmail.com>
2018-04-05 20:27:41 +0900

    Add Javadoc

--- src/main/java/jp/urgm/collectors/Collectors0.java
+++ src/main/java/jp/urgm/collectors/Collectors0.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import java.util.stream.Collector;
 
 /**
+ * Utility class that provide {@link Collector} implementation.
  * 
  * @author backpaper0
  *
@@ -13,6 +14,12 @@ public final class Collectors0 {
     private Collectors0() {
     }
 
+    /**
+     * Return first element of stream, or {@link Optional#empty()}.
+     * 
+     * @param <T> the type of stream elements
+     * @return first element of stream, or {@link Optional#empty()}
+     */
     public static <T> Collector<T, ?, Optional<T>> findFirst() {
         final Collector<T, FirstBox<T>, Optional<T>> collector = Collector.of(
                 FirstBox::new,
